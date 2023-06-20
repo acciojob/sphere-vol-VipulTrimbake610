fa4071b2ee8e0bad330a3326e807d6690b0eed13
Vipul Trimbake <130178066+VipulTrimbake610@users.noreply.github.com>
2023-06-20 12:09:08 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function volume_sphere() {
     //Write your code here
+	
+	const r = document.getElementById("radius").value;
+	const volume = document.getElementById("volume");
+
+	let area = 3.14 * r * r * 4 / 3;
+	volume.value=area;
+
+	
   
 } 
 
